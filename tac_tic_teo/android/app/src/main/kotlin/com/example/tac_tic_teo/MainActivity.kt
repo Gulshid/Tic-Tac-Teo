61af9bd4e643019f942b04a3e52029faf43b0713
Gulshid Zada <gulshidzada4@gmail.com>
2025-05-10 10:47:53 +0500

    Tac Tic Teo Game Setup is Started

---
+++ tac_tic_teo/android/app/src/main/kotlin/com/example/tac_tic_teo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tac_tic_teo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ tac_tic_teo/lib/main.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:tac_tic_teo/viewModel_/GameProvider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create:(_)=> Gameprovider()),
+          ],
+
+          child: Builder(
+            builder: (BuildContext context) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Unit Converter',
+                theme: ThemeData(
+                  applyElevationOverlayColor: true,
+                  brightness: Brightness.light,
+                  appBarTheme: AppBarTheme(color: Colors.teal),
+                  primarySwatch: Colors.blue,
+                  textTheme: Typography.englishLike2018.apply(
+                    fontSizeFactor: 1.sp,
+                  ),
+                ),
+          
+                // initialRoute: Routesname.home,
+                // onGenerateRoute: Routes.generate_Route,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
